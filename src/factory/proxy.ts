17116b6d1d996cdd076113ed216df88986ae6a8d
James <james@jmew.ca>
2022-10-27 05:02:20 -0400

    Fix proxy source

--- src/factory/proxy.ts
+++ src/factory/proxy.ts
@@ -1,5 +1,5 @@
-const corsAnywhere = process.env.cors || "http://0.0.0.0:8080";
-//const corsAnywhere = process.env.cors || "https://corsanywhere.up.railway.app";
+//const corsAnywhere = process.env.cors || "http://0.0.0.0:8080";
+const corsAnywhere = process.env.cors || "https://corsanywhere.up.railway.app";
 
 export const proxy = async (url: string, options: RequestInit = {}) => {
   const response = await fetch(`${corsAnywhere}/${url}`, {
