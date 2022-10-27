023ae04a95bd655dfd0155d28b055cfc60289b29
James <james@jmew.ca>
2022-10-26 23:44:21 -0400

    Remove default headers

--- src/factory/proxy.ts
+++ src/factory/proxy.ts
@@ -1,14 +1,7 @@
 const corsAnywhere = process.env.cors || "https://corsanywhere.up.railway.app";
 
 export const proxy = async (url: string, options: RequestInit = {}) => {
-  const { headers, ...rest } = options;
   return await fetch(`${corsAnywhere}/${url}`, {
-    ...rest,
-    headers: {
-      ...headers,
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-    credentials: "include",
+    ...options,
   });
 };
