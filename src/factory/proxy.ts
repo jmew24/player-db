ac9cc5b670509cf32df0b61879fd3d5484524479
James <james@jmew.ca>
2022-11-04 18:32:43 -0400

    Add proxy fetch timeout

--- src/factory/proxy.ts
+++ src/factory/proxy.ts
@@ -3,13 +3,30 @@ import { getProxyCache, setProxyCache } from "../factory/cache";
 //const corsAnywhere = process.env.cors || "http://0.0.0.0:8080";
 const corsAnywhere = process.env.cors || "https://corsanywhere.up.railway.app";
 
+type requestOptions = RequestInit & {
+  timeout?: number;
+};
+
+async function fetchWithTimeout(url: string, options: requestOptions = {}) {
+  const { timeout = 10000 } = options;
+
+  const controller = new AbortController();
+  const id = setTimeout(() => controller.abort(), timeout);
+  const response = await fetch(url, {
+    ...options,
+    signal: controller.signal,
+  });
+  clearTimeout(id);
+  return response;
+}
+
 export const proxy = async (url: string, options: RequestInit = {}) => {
   const cached = getProxyCache(url);
   if (cached) {
     return cached;
   }
 
-  const response = await fetch(`${corsAnywhere}/${url}`, {
+  const response = await fetchWithTimeout(`${corsAnywhere}/${url}`, {
     ...options,
   });
   return setProxyCache(url, response.json());
