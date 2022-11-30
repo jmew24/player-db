7d24cd0b2c788bbd352c195ea219778c004c61d3
James <james@jmew.ca>
2022-11-30 04:02:15 -0500

    Update Text

--- src/component/Search.tsx
+++ src/component/Search.tsx
@@ -32,7 +32,9 @@ const Search: FC<SearchProps> = ({ children }) => {
   return (
     <div className="flex min-h-screen w-full min-w-full flex-col items-center justify-center py-2">
       <main className="flex w-full min-w-full flex-1 flex-col items-center px-5 text-center">
-        <h1 className="text-6xl font-bold">Search For Players</h1>
+        <h1 className="text-6xl font-bold">
+          {searchType === "player" ? "Search For Players" : "Search For Teams"}
+        </h1>
         <form
           onSubmit={(e) => {
             e.preventDefault();
@@ -48,11 +50,13 @@ const Search: FC<SearchProps> = ({ children }) => {
               className="mx-2 w-1/6 justify-center  rounded border border-gray-500 bg-gray-700 p-2 text-center text-lg text-gray-200"
               value={searchType}
               onChange={(e) => handleSearchType(e.target.value)}
+              title="Search Type"
             >
               <option value="player">Player</option>
               <option value="team">Team</option>
             </select>
             <input
+              title="Search"
               type="text"
               value={search}
               placeholder={`Enter a ${searchType} name here...`}
@@ -60,6 +64,8 @@ const Search: FC<SearchProps> = ({ children }) => {
               className="mx-2 h-10 flex-grow justify-center rounded border-gray-500 bg-gray-700 px-5 text-center text-xl font-bold  text-gray-200 outline-double outline-1 outline-gray-500 focus:outline-none focus:ring"
             />
             <button
+              title="Search Button"
+              type="button"
               onClick={() => searchQuery()}
               className="mx-2 w-1/6 rounded-r bg-blue-500 px-8 py-2 text-white hover:bg-blue-600"
             >
