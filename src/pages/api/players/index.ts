c2c4e71c4e5b60d35f9d8e08f9fc99260f1b5b6a
James <james@jmew.ca>
2022-12-09 16:08:09 -0500

    Optimize API responses

--- src/pages/api/players/index.ts
+++ src/pages/api/players/index.ts
@@ -9,210 +9,207 @@ export default async function handle(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.query.sport) {
-    const sportQuery = (req.query.sport as string).trim();
-    const sport = await prisma.sport.findFirst({
+  if (!req.query.sport) return res.json([]);
+
+  const sportQuery = (req.query.sport as string).trim();
+  const sport = await prisma.sport.findFirst({
+    where: {
+      name: {
+        equals: sportQuery,
+        mode: "insensitive",
+      },
+      OR: [
+        {
+          name: {
+            equals: sportQuery.split(" ")[0],
+            mode: "insensitive",
+          },
+        },
+      ],
+    },
+  });
+
+  if (!sport) return res.json([]);
+
+  let results = [];
+  if (req.query.query) {
+    const nameQuery = (req.query.query as string).trim();
+    const fullName = nameQuery.replace(/\s+/g, " ");
+    const firstName = fullName.split(" ")[0]?.trim() ?? fullName;
+    const lastName = fullName.split(" ")[1]?.trim() ?? fullName;
+
+    results = await prisma.player.findMany({
       where: {
-        name: {
-          equals: sportQuery,
-          mode: "insensitive",
+        sport: {
+          id: sport.id,
+        },
+        AND:
+          fullName.split(" ").length > 1
+            ? {
+                AND: [
+                  {
+                    firstName: {
+                      contains: firstName,
+                      mode: "insensitive",
+                    },
+                    lastName: {
+                      contains: lastName,
+                      mode: "insensitive",
+                    },
+                  },
+                ],
+              }
+            : {
+                fullName: {
+                  contains: fullName,
+                  mode: "insensitive",
+                },
+              },
+      },
+      select: {
+        identifier: true,
+        updatedAt: true,
+        fullName: true,
+        firstName: true,
+        lastName: true,
+        position: true,
+        number: true,
+        headshotUrl: true,
+        linkUrl: true,
+        source: true,
+        team: {
+          select: {
+            identifier: true,
+            fullName: true,
+            city: true,
+            shortName: true,
+            abbreviation: true,
+            league: true,
+            source: true,
+          },
+        },
+        sport: {
+          select: {
+            id: true,
+            name: true,
+            updatedAt: true,
+          },
+        },
+      },
+    });
+  } else if (req.query.team) {
+    const teamQuery = (req.query.team as string).trim();
+
+    results = await prisma.player.findMany({
+      where: {
+        sport: {
+          id: sport.id,
         },
         OR: [
           {
-            name: {
-              equals: sportQuery.split(" ")[0],
-              mode: "insensitive",
+            team: {
+              fullName: {
+                contains: teamQuery,
+                mode: "insensitive",
+              },
+            },
+          },
+          {
+            team: {
+              shortName: {
+                contains: teamQuery,
+                mode: "insensitive",
+              },
+            },
+          },
+          {
+            team: {
+              city: {
+                contains: teamQuery,
+                mode: "insensitive",
+              },
+            },
+          },
+          {
+            team: {
+              abbreviation: {
+                contains: teamQuery,
+                mode: "insensitive",
+              },
             },
           },
         ],
       },
+      select: {
+        identifier: true,
+        updatedAt: true,
+        fullName: true,
+        firstName: true,
+        lastName: true,
+        position: true,
+        number: true,
+        headshotUrl: true,
+        linkUrl: true,
+        source: true,
+        team: {
+          select: {
+            identifier: true,
+            fullName: true,
+            city: true,
+            shortName: true,
+            abbreviation: true,
+            league: true,
+            source: true,
+          },
+        },
+        sport: {
+          select: {
+            id: true,
+            name: true,
+            updatedAt: true,
+          },
+        },
+      },
     });
-
-    if (sport && sport.id) {
-      if (req.query.query) {
-        const nameQuery = (req.query.query as string).trim();
-        const fullName = nameQuery.replaceAll("%20", " ");
-        const firstName = fullName.split(" ")[0]?.trim() ?? fullName;
-        const lastName = fullName.split(" ")[1]?.trim() ?? fullName;
-
-        const results = await prisma.player.findMany({
-          where: {
-            sport: {
-              id: sport?.id,
-            },
-            AND: [
-              fullName.split(" ").length > 1
-                ? {
-                    AND: [
-                      {
-                        firstName: {
-                          contains: firstName,
-                          mode: "insensitive",
-                        },
-                        lastName: {
-                          contains: lastName,
-                          mode: "insensitive",
-                        },
-                      },
-                    ],
-                  }
-                : {
-                    fullName: {
-                      contains: fullName,
-                      mode: "insensitive",
-                    },
-                  },
-            ],
-          },
+  } else {
+    results = await prisma.player.findMany({
+      where: {
+        sport: {
+          id: sport?.id,
+        },
+      },
+      select: {
+        identifier: true,
+        updatedAt: true,
+        fullName: true,
+        firstName: true,
+        lastName: true,
+        position: true,
+        number: true,
+        headshotUrl: true,
+        linkUrl: true,
+        source: true,
+        team: {
           select: {
             identifier: true,
-            updatedAt: true,
             fullName: true,
-            firstName: true,
-            lastName: true,
-            position: true,
-            number: true,
-            headshotUrl: true,
-            linkUrl: true,
+            city: true,
+            shortName: true,
+            abbreviation: true,
+            league: true,
             source: true,
-            team: {
-              select: {
-                identifier: true,
-                fullName: true,
-                city: true,
-                shortName: true,
-                abbreviation: true,
-                league: true,
-                source: true,
-              },
-            },
-            sport: {
-              select: {
-                id: true,
-                name: true,
-                updatedAt: true,
-              },
-            },
           },
-        });
-        return res.json(results);
-      } else if (req.query.team) {
-        const teamQuery = (req.query.team as string).trim();
-
-        const results = await prisma.player.findMany({
-          where: {
-            sport: {
-              id: sport?.id,
-            },
-            OR: [
-              {
-                team: {
-                  fullName: {
-                    contains: teamQuery,
-                    mode: "insensitive",
-                  },
-                },
-              },
-              {
-                team: {
-                  shortName: {
-                    contains: teamQuery,
-                    mode: "insensitive",
-                  },
-                },
-              },
-              {
-                team: {
-                  city: {
-                    contains: teamQuery,
-                    mode: "insensitive",
-                  },
-                },
-              },
-              {
-                team: {
-                  abbreviation: {
-                    contains: teamQuery,
-                    mode: "insensitive",
-                  },
-                },
-              },
-            ],
-          },
+        },
+        sport: {
           select: {
-            identifier: true,
+            id: true,
+            name: true,
             updatedAt: true,
-            fullName: true,
-            firstName: true,
-            lastName: true,
-            position: true,
-            number: true,
-            headshotUrl: true,
-            linkUrl: true,
-            source: true,
-            team: {
-              select: {
-                identifier: true,
-                fullName: true,
-                city: true,
-                shortName: true,
-                abbreviation: true,
-                league: true,
-                source: true,
-              },
-            },
-            sport: {
-              select: {
-                id: true,
-                name: true,
-                updatedAt: true,
-              },
-            },
           },
-        });
-        return res.json(results);
-      } else {
-        const results = await prisma.player.findMany({
-          where: {
-            sport: {
-              id: sport?.id,
-            },
-          },
-          select: {
-            identifier: true,
-            updatedAt: true,
-            fullName: true,
-            firstName: true,
-            lastName: true,
-            position: true,
-            number: true,
-            headshotUrl: true,
-            linkUrl: true,
-            source: true,
-            team: {
-              select: {
-                identifier: true,
-                fullName: true,
-                city: true,
-                shortName: true,
-                abbreviation: true,
-                league: true,
-                source: true,
-              },
-            },
-            sport: {
-              select: {
-                id: true,
-                name: true,
-                updatedAt: true,
-              },
-            },
-          },
-        });
-        return res.json(results);
-      }
-    }
+        },
+      },
+    });
   }
 
-  return res.json([]);
+  return res.json(results);
 }
--- src/pages/api/teams/index.ts
+++ src/pages/api/teams/index.ts
@@ -2,118 +2,119 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 import prisma from "@lib/prisma";
 
+// GET /api/teams?sport=:sport
 // GET /api/teams?sport=:sport&query=:query
 export default async function handle(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.query.sport) {
-    const sport = await prisma.sport.findFirst({
+  if (!req.query.sport) return res.json([]);
+
+  const sportQuery = (req.query.sport as string).trim();
+  const sport = await prisma.sport.findFirst({
+    where: {
+      name: {
+        equals: sportQuery,
+        mode: "insensitive",
+      },
+    },
+  });
+
+  if (!sport || !sport.id) return res.json([]);
+
+  let results = [];
+  if (req.query.query) {
+    const nameQuery = (req.query.query as string).trim();
+    results = await prisma.team.findMany({
       where: {
-        name: {
-          equals: req.query.sport as string,
-          mode: "insensitive",
+        sport: {
+          id: sport?.id,
+        },
+        OR: [
+          {
+            fullName: {
+              contains: nameQuery,
+              mode: "insensitive",
+            },
+          },
+          {
+            city: {
+              contains: nameQuery,
+              mode: "insensitive",
+            },
+          },
+          {
+            shortName: {
+              contains: nameQuery,
+              mode: "insensitive",
+            },
+          },
+          {
+            abbreviation: {
+              contains: nameQuery,
+              mode: "insensitive",
+            },
+          },
+        ],
+      },
+      select: {
+        identifier: true,
+        updatedAt: true,
+        fullName: true,
+        city: true,
+        shortName: true,
+        abbreviation: true,
+        league: true,
+        source: true,
+        sport: {
+          select: {
+            id: true,
+            name: true,
+            updatedAt: true,
+          },
+        },
+        players: {
+          select: {
+            identifier: true,
+            updatedAt: true,
+            fullName: true,
+            firstName: true,
+            lastName: true,
+            position: true,
+            number: true,
+            headshotUrl: true,
+            linkUrl: true,
+            source: true,
+          },
         },
       },
     });
-
-    if (sport && sport.id) {
-      if (req.query.query) {
-        const nameQuery = (req.query.query as string).trim();
-        const results = await prisma.team.findMany({
-          where: {
-            sport: {
-              id: sport?.id,
-            },
-            OR: [
-              {
-                fullName: {
-                  contains: nameQuery,
-                  mode: "insensitive",
-                },
-              },
-              {
-                city: {
-                  contains: nameQuery,
-                  mode: "insensitive",
-                },
-              },
-              {
-                shortName: {
-                  contains: nameQuery,
-                  mode: "insensitive",
-                },
-              },
-              {
-                abbreviation: {
-                  contains: nameQuery,
-                  mode: "insensitive",
-                },
-              },
-            ],
-          },
+  } else {
+    results = await prisma.team.findMany({
+      where: {
+        sport: {
+          id: sport?.id,
+        },
+      },
+      select: {
+        identifier: true,
+        updatedAt: true,
+        fullName: true,
+        city: true,
+        shortName: true,
+        abbreviation: true,
+        league: true,
+        source: true,
+        sport: {
           select: {
-            identifier: true,
+            id: true,
+            name: true,
             updatedAt: true,
-            fullName: true,
-            city: true,
-            shortName: true,
-            abbreviation: true,
-            league: true,
-            source: true,
-            sport: {
-              select: {
-                id: true,
-                name: true,
-                updatedAt: true,
-              },
-            },
-            players: {
-              select: {
-                identifier: true,
-                updatedAt: true,
-                fullName: true,
-                firstName: true,
-                lastName: true,
-                position: true,
-                number: true,
-                headshotUrl: true,
-                linkUrl: true,
-                source: true,
-              },
-            },
           },
-        });
-        return res.json(results);
-      } else {
-        const results = await prisma.team.findMany({
-          where: {
-            sport: {
-              id: sport?.id,
-            },
-          },
-          select: {
-            identifier: true,
-            updatedAt: true,
-            fullName: true,
-            city: true,
-            shortName: true,
-            abbreviation: true,
-            league: true,
-            source: true,
-            sport: {
-              select: {
-                id: true,
-                name: true,
-                updatedAt: true,
-              },
-            },
-          },
-        });
-        return res.json(results);
-      }
-    }
+        },
+      },
+    });
   }
 
-  return res.json([]);
+  return res.json(results);
 }
