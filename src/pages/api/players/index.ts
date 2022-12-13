ed6981889e959a7e547a1c6fda031f25665698d0
James <james@jmew.ca>
2022-12-13 00:05:02 -0500

    Remove redis

--- src/pages/api/players/index.ts
+++ src/pages/api/players/index.ts
@@ -1,8 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { Sport } from "@prisma/client";
 
 import prisma from "@lib/prisma";
-import redisClient from "@lib/redis";
 
 // GET /api/players?sport=:sport
 // GET /api/players?sport=:sport&query=:query
@@ -14,49 +12,26 @@ export default async function handle(
   if (!req.query.sport) return res.json([]);
 
   const sportQuery = (req.query.sport as string).trim();
-  const sportCacheStr =
-    (await redisClient.get(`sportCache:${sportQuery.toLowerCase()}`)) ?? "{}";
-  const sportCache = JSON.parse(sportCacheStr);
-  let sport: Sport | null = null;
 
-  if (sportCacheStr !== "{}") {
-    if (
-      sportCache &&
-      sportCache?.name.toLowerCase() === sportQuery.toLowerCase()
-    ) {
-      sport = { ...sportCache } as Sport;
-    }
-  }
-
-  if (!sport) {
-    sport = await prisma.sport.findFirst({
-      where: {
-        name: {
-          equals: sportQuery,
-          mode: "insensitive",
-        },
-        OR: [
-          {
-            name: {
-              equals: sportQuery.split(" ")[0],
-              mode: "insensitive",
-            },
-          },
-        ],
+  const sport = await prisma.sport.findFirst({
+    where: {
+      name: {
+        equals: sportQuery,
+        mode: "insensitive",
       },
-    });
-    if (sport)
-      redisClient.set(
-        `sportCache:${sportQuery.toLowerCase()}`,
-        JSON.stringify({ ...sportCache, ...sport })
-      );
-  }
+      OR: [
+        {
+          name: {
+            equals: sportQuery.split(" ")[0],
+            mode: "insensitive",
+          },
+        },
+      ],
+    },
+  });
 
   if (!sport) return res.json([]);
 
-  const resultCacheStr =
-    (await redisClient.get(`playerCache:${sportQuery.toLowerCase()}`)) ?? "{}";
-  const playerCache = JSON.parse(resultCacheStr);
   let results = [];
 
   if (req.query.query) {
@@ -65,243 +40,177 @@ export default async function handle(
     const firstName = fullName.split(" ")[0]?.trim() ?? fullName;
     const lastName = fullName.split(" ")[1]?.trim() ?? fullName;
 
-    if (resultCacheStr !== "{}") {
-      if (playerCache) {
-        const fullNameLower = fullName.toLowerCase();
-        const firstNameLower = firstName.toLowerCase();
-        const lastNameLower = lastName.toLowerCase();
-        for (const index in playerCache) {
-          const player = playerCache[index];
-          if (
-            player?.sport?.id === sport.id &&
-            (player?.fullName?.toLowerCase().includes(fullNameLower) ||
-              (player?.firstName?.toLowerCase().includes(firstNameLower) &&
-                player?.lastName?.toLowerCase().includes(lastNameLower)))
-          ) {
-            results.push(player);
-          }
-        }
-      }
-    }
-
-    if (results.length === 0) {
-      results = await prisma.player.findMany({
-        where: {
-          sport: {
-            id: sport.id,
-          },
-          AND:
-            fullName.split(" ").length > 1
-              ? {
-                  AND: [
-                    {
-                      firstName: {
-                        contains: firstName,
-                        mode: "insensitive",
-                      },
-                      lastName: {
-                        contains: lastName,
-                        mode: "insensitive",
-                      },
+    results = await prisma.player.findMany({
+      where: {
+        sport: {
+          id: sport.id,
+        },
+        AND:
+          fullName.split(" ").length > 1
+            ? {
+                AND: [
+                  {
+                    firstName: {
+                      contains: firstName,
+                      mode: "insensitive",
                     },
-                  ],
-                }
-              : {
-                  fullName: {
-                    contains: fullName,
-                    mode: "insensitive",
+                    lastName: {
+                      contains: lastName,
+                      mode: "insensitive",
+                    },
                   },
+                ],
+              }
+            : {
+                fullName: {
+                  contains: fullName,
+                  mode: "insensitive",
                 },
-        },
-        select: {
-          identifier: true,
-          updatedAt: true,
-          fullName: true,
-          firstName: true,
-          lastName: true,
-          position: true,
-          number: true,
-          headshotUrl: true,
-          linkUrl: true,
-          source: true,
-          team: {
-            select: {
-              identifier: true,
-              fullName: true,
-              city: true,
-              shortName: true,
-              abbreviation: true,
-              league: true,
-              source: true,
-            },
-          },
-          sport: {
-            select: {
-              id: true,
-              name: true,
-              updatedAt: true,
-            },
+              },
+      },
+      select: {
+        identifier: true,
+        updatedAt: true,
+        fullName: true,
+        firstName: true,
+        lastName: true,
+        position: true,
+        number: true,
+        headshotUrl: true,
+        linkUrl: true,
+        source: true,
+        team: {
+          select: {
+            identifier: true,
+            fullName: true,
+            city: true,
+            shortName: true,
+            abbreviation: true,
+            league: true,
+            source: true,
           },
         },
-      });
-      if (results.length > 0)
-        redisClient.set(
-          `playerCache:${sportQuery.toLowerCase()}`,
-          JSON.stringify({ ...playerCache, ...results })
-        );
-    }
+        sport: {
+          select: {
+            id: true,
+            name: true,
+            updatedAt: true,
+          },
+        },
+      },
+    });
   } else if (req.query.team) {
     const teamQuery = (req.query.team as string).trim();
 
-    if (resultCacheStr !== "{}") {
-      if (playerCache && playerCache) {
-        const teamQueryLower = teamQuery.toLowerCase();
-        for (const index in playerCache) {
-          const player = playerCache[index];
-          if (
-            player?.sport?.id === sport.id &&
-            (player?.team?.fullName.toLowerCase().includes(teamQueryLower) ||
-              player?.team?.shortName.toLowerCase().includes(teamQueryLower) ||
-              player?.team?.city.toLowerCase().includes(teamQueryLower) ||
-              player?.team?.abbreviation.toLowerCase().includes(teamQueryLower))
-          ) {
-            results.push(player);
-          }
-        }
-      }
-    }
-
-    if (results.length === 0) {
-      results = await prisma.player.findMany({
-        where: {
-          sport: {
-            id: sport.id,
-          },
-          OR: [
-            {
-              team: {
-                fullName: {
-                  contains: teamQuery,
-                  mode: "insensitive",
-                },
-              },
-            },
-            {
-              team: {
-                shortName: {
-                  contains: teamQuery,
-                  mode: "insensitive",
-                },
-              },
-            },
-            {
-              team: {
-                city: {
-                  contains: teamQuery,
-                  mode: "insensitive",
-                },
-              },
-            },
-            {
-              team: {
-                abbreviation: {
-                  contains: teamQuery,
-                  mode: "insensitive",
-                },
-              },
-            },
-          ],
+    results = await prisma.player.findMany({
+      where: {
+        sport: {
+          id: sport.id,
         },
-        select: {
-          identifier: true,
-          updatedAt: true,
-          fullName: true,
-          firstName: true,
-          lastName: true,
-          position: true,
-          number: true,
-          headshotUrl: true,
-          linkUrl: true,
-          source: true,
-          team: {
-            select: {
-              identifier: true,
-              fullName: true,
-              city: true,
-              shortName: true,
-              abbreviation: true,
-              league: true,
-              source: true,
+        OR: [
+          {
+            team: {
+              fullName: {
+                contains: teamQuery,
+                mode: "insensitive",
+              },
             },
           },
-          sport: {
-            select: {
-              id: true,
-              name: true,
-              updatedAt: true,
+          {
+            team: {
+              shortName: {
+                contains: teamQuery,
+                mode: "insensitive",
+              },
             },
+          },
+          {
+            team: {
+              city: {
+                contains: teamQuery,
+                mode: "insensitive",
+              },
+            },
+          },
+          {
+            team: {
+              abbreviation: {
+                contains: teamQuery,
+                mode: "insensitive",
+              },
+            },
+          },
+        ],
+      },
+      select: {
+        identifier: true,
+        updatedAt: true,
+        fullName: true,
+        firstName: true,
+        lastName: true,
+        position: true,
+        number: true,
+        headshotUrl: true,
+        linkUrl: true,
+        source: true,
+        team: {
+          select: {
+            identifier: true,
+            fullName: true,
+            city: true,
+            shortName: true,
+            abbreviation: true,
+            league: true,
+            source: true,
           },
         },
-      });
-      if (results.length > 0)
-        redisClient.set(
-          `playerCache:${sportQuery.toLowerCase()}`,
-          JSON.stringify({ ...playerCache, ...results })
-        );
-    }
+        sport: {
+          select: {
+            id: true,
+            name: true,
+            updatedAt: true,
+          },
+        },
+      },
+    });
   } else {
-    if (resultCacheStr !== "{}") {
-      if (playerCache && playerCache) {
-        for (const player of playerCache) {
-          if (player?.sportId === sport.id) results.push(player);
-        }
-      }
-    }
-
-    if (results.length === 0) {
-      results = await prisma.player.findMany({
-        where: {
-          sport: {
-            id: sport?.id,
+    results = await prisma.player.findMany({
+      where: {
+        sport: {
+          id: sport?.id,
+        },
+      },
+      select: {
+        identifier: true,
+        updatedAt: true,
+        fullName: true,
+        firstName: true,
+        lastName: true,
+        position: true,
+        number: true,
+        headshotUrl: true,
+        linkUrl: true,
+        source: true,
+        team: {
+          select: {
+            identifier: true,
+            fullName: true,
+            city: true,
+            shortName: true,
+            abbreviation: true,
+            league: true,
+            source: true,
           },
         },
-        select: {
-          identifier: true,
-          updatedAt: true,
-          fullName: true,
-          firstName: true,
-          lastName: true,
-          position: true,
-          number: true,
-          headshotUrl: true,
-          linkUrl: true,
-          source: true,
-          team: {
-            select: {
-              identifier: true,
-              fullName: true,
-              city: true,
-              shortName: true,
-              abbreviation: true,
-              league: true,
-              source: true,
-            },
-          },
-          sport: {
-            select: {
-              id: true,
-              name: true,
-              updatedAt: true,
-            },
+        sport: {
+          select: {
+            id: true,
+            name: true,
+            updatedAt: true,
           },
         },
-      });
-      if (results.length > 0)
-        redisClient.set(
-          `playerCache:${sportQuery.toLowerCase()}`,
-          JSON.stringify({ ...playerCache, ...results })
-        );
-    }
+      },
+    });
   }
 
   return res.json(results);
--- src/pages/api/teams/index.ts
+++ src/pages/api/teams/index.ts
@@ -1,8 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { Sport } from "@prisma/client";
 
 import prisma from "@lib/prisma";
-import redisClient from "@lib/redis";
 
 // GET /api/teams?sport=:sport
 // GET /api/teams?sport=:sport&query=:query
@@ -13,184 +11,120 @@ export default async function handle(
   if (!req.query.sport) return res.json([]);
 
   const sportQuery = (req.query.sport as string).trim();
-  const sportCacheStr =
-    (await redisClient.get(`sportCache:${sportQuery.toLowerCase()}`)) ?? "{}";
-  const sportCache = JSON.parse(sportCacheStr);
-  let sport: Sport | null = null;
 
-  if (sportCacheStr !== "{}") {
-    if (
-      sportCache &&
-      sportCache?.name.toLowerCase() === sportQuery.toLowerCase()
-    ) {
-      sport = { ...sportCache } as Sport;
-    }
-  }
-
-  if (!sport) {
-    sport = await prisma.sport.findFirst({
-      where: {
-        name: {
-          equals: sportQuery,
-          mode: "insensitive",
-        },
-        OR: [
-          {
-            name: {
-              equals: sportQuery.split(" ")[0],
-              mode: "insensitive",
-            },
-          },
-        ],
+  const sport = await prisma.sport.findFirst({
+    where: {
+      name: {
+        equals: sportQuery,
+        mode: "insensitive",
       },
-    });
-    if (sport)
-      redisClient.set(
-        `sportCache:${sportQuery.toLowerCase()}`,
-        JSON.stringify({ ...sportCache, ...sport })
-      );
-  }
+      OR: [
+        {
+          name: {
+            equals: sportQuery.split(" ")[0],
+            mode: "insensitive",
+          },
+        },
+      ],
+    },
+  });
 
   if (!sport) return res.json([]);
 
-  const resultCacheStr =
-    (await redisClient.get(`teamCache:${sportQuery.toLowerCase()}`)) ?? "{}";
-  const teamCache = JSON.parse(resultCacheStr);
   let results = [];
 
   if (req.query.query) {
     const nameQuery = (req.query.query as string).trim();
 
-    if (resultCacheStr !== "{}") {
-      if (teamCache) {
-        const teamQueryLower = nameQuery.toLowerCase();
-        for (const index in teamCache) {
-          const team = teamCache[index];
-          if (
-            team?.sport?.id === sport.id &&
-            (team?.fullName.toLowerCase().includes(teamQueryLower) ||
-              team?.shortName.toLowerCase().includes(teamQueryLower) ||
-              team?.city.toLowerCase().includes(teamQueryLower) ||
-              team?.abbreviation.toLowerCase().includes(teamQueryLower))
-          ) {
-            results.push(team);
-          }
-        }
-      }
-    }
-
-    if (results.length === 0) {
-      results = await prisma.team.findMany({
-        where: {
-          sport: {
-            id: sport?.id,
-          },
-          OR: [
-            {
-              fullName: {
-                contains: nameQuery,
-                mode: "insensitive",
-              },
-            },
-            {
-              city: {
-                contains: nameQuery,
-                mode: "insensitive",
-              },
-            },
-            {
-              shortName: {
-                contains: nameQuery,
-                mode: "insensitive",
-              },
-            },
-            {
-              abbreviation: {
-                contains: nameQuery,
-                mode: "insensitive",
-              },
-            },
-          ],
+    results = await prisma.team.findMany({
+      where: {
+        sport: {
+          id: sport?.id,
         },
-        select: {
-          identifier: true,
-          updatedAt: true,
-          fullName: true,
-          city: true,
-          shortName: true,
-          abbreviation: true,
-          league: true,
-          source: true,
-          sport: {
-            select: {
-              id: true,
-              name: true,
-              updatedAt: true,
+        OR: [
+          {
+            fullName: {
+              contains: nameQuery,
+              mode: "insensitive",
             },
           },
-          players: {
-            select: {
-              identifier: true,
-              updatedAt: true,
-              fullName: true,
-              firstName: true,
-              lastName: true,
-              position: true,
-              number: true,
-              headshotUrl: true,
-              linkUrl: true,
-              source: true,
+          {
+            city: {
+              contains: nameQuery,
+              mode: "insensitive",
             },
+          },
+          {
+            shortName: {
+              contains: nameQuery,
+              mode: "insensitive",
+            },
+          },
+          {
+            abbreviation: {
+              contains: nameQuery,
+              mode: "insensitive",
+            },
+          },
+        ],
+      },
+      select: {
+        identifier: true,
+        updatedAt: true,
+        fullName: true,
+        city: true,
+        shortName: true,
+        abbreviation: true,
+        league: true,
+        source: true,
+        sport: {
+          select: {
+            id: true,
+            name: true,
+            updatedAt: true,
           },
         },
-      });
-      if (results.length > 0)
-        redisClient.set(
-          `teamCache:${sportQuery.toLowerCase()}`,
-          JSON.stringify({ ...teamCache, ...results })
-        );
-    }
+        players: {
+          select: {
+            identifier: true,
+            updatedAt: true,
+            fullName: true,
+            firstName: true,
+            lastName: true,
+            position: true,
+            number: true,
+            headshotUrl: true,
+            linkUrl: true,
+            source: true,
+          },
+        },
+      },
+    });
   } else {
-    if (resultCacheStr !== "{}") {
-      if (teamCache) {
-        for (const index in teamCache) {
-          const team = teamCache[index];
-          if (team?.sport.id === sport.id) results.push(team);
-        }
-      }
-    }
-
-    if (results.length === 0) {
-      results = await prisma.team.findMany({
-        where: {
-          sport: {
-            id: sport?.id,
+    results = await prisma.team.findMany({
+      where: {
+        sport: {
+          id: sport?.id,
+        },
+      },
+      select: {
+        identifier: true,
+        updatedAt: true,
+        fullName: true,
+        city: true,
+        shortName: true,
+        abbreviation: true,
+        league: true,
+        source: true,
+        sport: {
+          select: {
+            id: true,
+            name: true,
+            updatedAt: true,
           },
         },
-        select: {
-          identifier: true,
-          updatedAt: true,
-          fullName: true,
-          city: true,
-          shortName: true,
-          abbreviation: true,
-          league: true,
-          source: true,
-          sport: {
-            select: {
-              id: true,
-              name: true,
-              updatedAt: true,
-            },
-          },
-        },
-      });
-      if (results.length > 0)
-        redisClient.set(
-          `teamCache:${sportQuery.toLowerCase()}`,
-          JSON.stringify({ ...teamCache, ...results })
-        );
-    }
+      },
+    });
   }
 
   return res.json(results);
