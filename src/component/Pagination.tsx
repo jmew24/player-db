d6e825cc8c932ef36a8007aa99cab741f20e5d6b
James <james@jmew.ca>
2022-12-16 21:00:25 -0500

    Update Pagination

--- src/component/Pagination.tsx
+++ src/component/Pagination.tsx
@@ -13,6 +13,89 @@ const Pagination: FC<PaginationProps> = ({
     [data.count, data.end]
   );
 
+  const controls = useMemo(() => {
+    return pagesDisplay.length > 1 ? (
+      <ul className="inline-flex">
+        <li className={`${selected === 0 && "disabled"}`}>
+          <button
+            className={`${
+              selected > 0
+                ? "focus:shadow-outline h-10 rounded-l-lg border border-r-0 border-gray-500 bg-gray-600 px-5 text-white transition-colors duration-150 hover:bg-gray-500"
+                : "h-10 cursor-not-allowed rounded-l-lg border border-r-0 border-gray-500  bg-gray-600 px-5 text-white opacity-50 transition-colors duration-150 hover:bg-gray-500"
+            }`}
+            onClick={() => selected > 0 && setPage(0)}
+            disabled={selected === 0 ? true : false}
+          >
+            &laquo;
+          </button>
+        </li>
+        <li className={`${selected === 0 && "disabled"}`}>
+          <button
+            className={`${
+              selected > 0
+                ? "focus:shadow-outline h-10 border border-gray-500 bg-gray-600 px-5 text-white transition-colors duration-150 hover:bg-gray-500"
+                : "h-10 cursor-not-allowed border border-gray-500  bg-gray-600 px-5 text-white opacity-50 transition-colors duration-150 hover:bg-gray-500"
+            }`}
+            onClick={() =>
+              selected > 0 && setPage(selected - 1 < 0 ? 0 : selected - 1)
+            }
+            disabled={selected === 0 ? true : false}
+          >
+            {"<"}
+          </button>
+        </li>
+        <li key={`page-${selected}`}>
+          <button
+            className={`focus:shadow-outline h-10 cursor-not-allowed border border-r-0 border-gray-500 bg-gray-600 px-5 text-white opacity-50 transition-colors duration-150 hover:bg-gray-500`}
+            disabled={true}
+          >
+            {selected + 1}
+          </button>
+        </li>
+        {/*pagesDisplay.map((page) => (
+        <li key={`page-${page}`}>
+          <button
+            className={`focus:shadow-outline h-10 border border-r-0 border-gray-500 ${
+              selected === page
+                ? "bg-white text-gray-600"
+                : "bg-gray-600 text-white"
+            } px-5 transition-colors duration-150`}
+            onClick={() => setPage(page)}
+          >
+            {page + 1}
+          </button>
+        </li>
+          ))*/}
+        <li>
+          <button
+            className={`${
+              selected < pages - 1
+                ? "focus:shadow-outline h-10 border border-gray-500 bg-gray-600 px-5 text-white transition-colors duration-150 hover:bg-gray-500"
+                : "h-10 cursor-not-allowed border border-gray-500  bg-gray-600 px-5 text-white opacity-50 transition-colors duration-150 hover:bg-gray-500"
+            }`}
+            onClick={() => selected < pages - 1 && setPage(selected + 1)}
+          >
+            {">"}
+          </button>
+        </li>
+        <li>
+          <button
+            className={`${
+              selected < pages - 1
+                ? "focus:shadow-outline h-10 rounded-r-lg border border-gray-500 bg-gray-600 px-5 text-white transition-colors duration-150 hover:bg-gray-500"
+                : "h-10 cursor-not-allowed rounded-r-lg border border-gray-500  bg-gray-600 px-5 text-white opacity-50 transition-colors duration-150 hover:bg-gray-500"
+            }`}
+            onClick={() =>
+              selected < pages - 1 && setPage(pagesArray[pagesArray.length - 1])
+            }
+          >
+            &raquo;
+          </button>
+        </li>
+      </ul>
+    ) : null;
+  }, [pages, pagesArray, pagesDisplay.length, selected, setPage]);
+
   return (
     <nav aria-label="Page navigation" className="w-full">
       {data.count > 0 ? (
@@ -29,87 +112,7 @@ const Pagination: FC<PaginationProps> = ({
           No Results
         </p>
       )}
-      {pagesDisplay.length > 1 && (
-        <ul className="inline-flex">
-          <li className={`${selected === 0 && "disabled"}`}>
-            <button
-              className={`${
-                selected > 0
-                  ? "focus:shadow-outline h-10 rounded-l-lg border border-r-0 border-gray-500 bg-gray-600 px-5 text-white transition-colors duration-150 hover:bg-gray-500"
-                  : "h-10 cursor-not-allowed rounded-l-lg border border-r-0 border-gray-500  bg-gray-600 px-5 text-white opacity-50 transition-colors duration-150 hover:bg-gray-500"
-              }`}
-              onClick={() => selected > 0 && setPage(0)}
-              disabled={selected === 0 ? true : false}
-            >
-              &laquo;
-            </button>
-          </li>
-          <li className={`${selected === 0 && "disabled"}`}>
-            <button
-              className={`${
-                selected > 0
-                  ? "focus:shadow-outline h-10 border border-gray-500 bg-gray-600 px-5 text-white transition-colors duration-150 hover:bg-gray-500"
-                  : "h-10 cursor-not-allowed border border-gray-500  bg-gray-600 px-5 text-white opacity-50 transition-colors duration-150 hover:bg-gray-500"
-              }`}
-              onClick={() =>
-                selected > 0 && setPage(selected - 1 < 0 ? 0 : selected - 1)
-              }
-              disabled={selected === 0 ? true : false}
-            >
-              {"<"}
-            </button>
-          </li>
-          <li key={`page-${selected}`}>
-            <button
-              className={`focus:shadow-outline h-10 cursor-not-allowed border border-r-0 border-gray-500 bg-gray-600 px-5 text-white opacity-50 transition-colors duration-150 hover:bg-gray-500`}
-              disabled={true}
-            >
-              {selected + 1}
-            </button>
-          </li>
-          {/*pagesDisplay.map((page) => (
-        <li key={`page-${page}`}>
-          <button
-            className={`focus:shadow-outline h-10 border border-r-0 border-gray-500 ${
-              selected === page
-                ? "bg-white text-gray-600"
-                : "bg-gray-600 text-white"
-            } px-5 transition-colors duration-150`}
-            onClick={() => setPage(page)}
-          >
-            {page + 1}
-          </button>
-        </li>
-          ))*/}
-          <li>
-            <button
-              className={`${
-                selected < pages - 1
-                  ? "focus:shadow-outline h-10 border border-gray-500 bg-gray-600 px-5 text-white transition-colors duration-150 hover:bg-gray-500"
-                  : "h-10 cursor-not-allowed border border-gray-500  bg-gray-600 px-5 text-white opacity-50 transition-colors duration-150 hover:bg-gray-500"
-              }`}
-              onClick={() => selected < pages - 1 && setPage(selected + 1)}
-            >
-              {">"}
-            </button>
-          </li>
-          <li>
-            <button
-              className={`${
-                selected < pages - 1
-                  ? "focus:shadow-outline h-10 rounded-r-lg border border-gray-500 bg-gray-600 px-5 text-white transition-colors duration-150 hover:bg-gray-500"
-                  : "h-10 cursor-not-allowed rounded-r-lg border border-gray-500  bg-gray-600 px-5 text-white opacity-50 transition-colors duration-150 hover:bg-gray-500"
-              }`}
-              onClick={() =>
-                selected < pages - 1 &&
-                setPage(pagesArray[pagesArray.length - 1])
-              }
-            >
-              &raquo;
-            </button>
-          </li>
-        </ul>
-      )}
+      {controls}
     </nav>
   );
 };
